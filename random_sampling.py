bd0dc0df8cb54a3ad1a34db26d00f94b34c36506
John Tsang <wk3tsang@uwaterloo.ca>
2018-01-11 12:00:56 -0500

    Create random_sampling.py

---
+++ random_sampling.py
@@ -0,0 +1,20 @@
+# Author : John Tsang
+# Date   : January 11, 2018
+# Purpose: random draw from sample (with replacement and without replacement) 
+def random_sampling(sample, size, is_replacement = True):
+    import random
+    len_sample = len(sample)
+    if (len_sample <= 0):
+        raise ValueError('Size of sample must be greater than or equal to 1.')
+    if (is_replacement == True):
+        rt_lst = [random.choice(sample) for index in range(0,size)]
+        return rt_lst
+    else:
+        if (size > is_replacement):
+            raise ValueError('size must be less than or equal to size of sample for without replacement.')
+        rt_lst = []
+        for index in range(0,size):
+            draw = random.choice(sample)
+            sample.remove(draw)
+            rt_lst.append(draw)
+        return rt_lst
